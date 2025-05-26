497a4ebb1892ab7c6c8e833bbd47874c5104013d
Oskar Forreiter <forreitero@gmail.com>
2025-05-26 21:33:23 +0200

    fix request to static domain

--- request.py
+++ request.py
@@ -1,13 +1,13 @@
 import requests
 
-response = requests.post("http://localhost:8080/chat", json={
+response = requests.post("https://real-large-cricket.ngrok-free.app/chat", json={
     "history": [
         {"role": "user", "message": "Czym jest Maszyna W?"},
         {"role": "assistant", "message": "Maszyna W to prosta, edukacyjna architektura komputera wykorzystywana do nauki programowania w asemblerze."},
         {"role": "user", "message": "Do czego służy instrukcja 'POB' w Maszynie W?"},
         {"role": "assistant", "message": "Instrukcja 'POB' ładuje wartość ze wskazanego adresu pamięci do akumulatora."},
         {"role": "user", "message": "Jak zsumować wszystkie elementy tablicy w asemblerze Maszyny W?"}
-    ],
+    ]
 })
 
 print(response.json()["response"])
